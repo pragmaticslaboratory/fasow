1c3c3a549230a62f551a5b3f846d7f9655ad5426
D1eku <dieku25ikari@gmail.com>
2025-07-15 00:54:10 -0400

    Rename "Experiment" references to "Calibration" in Calibration.ts
    This refactor updates all relevant class names, comments, method names, and parameters from "Experiment" to "Calibration" for consistency and clarity. It ensures alignment with the intended purpose of the "Calibration" class across the module.

--- fasow-api/src/fasow/fasow/abm/Calibration.ts
+++ fasow-api/src/fasow/fasow/abm/Calibration.ts
@@ -1,13 +1,13 @@
 import FASOW from '../FASOW';
 import CalibrationConfig from '../config/config/CalibrationConfig';
-import MetaExperimentConfig from '../config/metaconfig/MetaExperimentConfig';
+import MetaCalibrationConfig from '../config/metaconfig/MetaCalibrationConfig';
 import Simulation from './Simulation';
 import ICalibrationCreator from './interfaces/Experiment/ICalibrationCreator';
 import ICalibrationStrategy from './interfaces/Experiment/ICalibrationStrategy';
 import Repetitions from '../timekeeper/Repetitions';
 
 /**
- * The Experiment abstract class allow to the user to Implement and Configure an Experiment overriding the Strategy Method
+ * The Calibration abstract class allow to the user to Implement and Configure an Calibration overriding the Strategy Method
  */
 export default abstract class Calibration
   implements
@@ -30,13 +30,13 @@ export default abstract class Calibration
   }
 
   /**
-   * Run the Experiment,initializing the model and starting the simulation
+   * Run the Calibration,initializing the model and starting the simulation
    * */
   run() {
     this.setRepetition(0);
     this.initialize();
     console.log(
-      'Ended Initialization --> On Experiment.run(), currentRepetition  is: ',
+      'Ended Initialization --> On Calibration.run(), currentRepetition  is: ',
       this.getRepetition() + 1,
       ' of (',
       this.getMaxRepetition(),
@@ -53,7 +53,7 @@ export default abstract class Calibration
       if (this.canNextRepetition()) {
         this.initialize();
         console.log(
-          'Ended Initialization --> On Experiment.run(), currentRepetition  is: ',
+          'Ended Initialization --> On Calibration.run(), currentRepetition  is: ',
           this.getRepetition() + 1,
           ' of (',
           this.getMaxRepetition(),
@@ -61,7 +61,7 @@ export default abstract class Calibration
         );
       }
     }
-    console.log('Ending Experiment...');
+    console.log('Ending Calibration...');
   }
 
   /**
@@ -78,10 +78,10 @@ export default abstract class Calibration
   abstract createCalibration(): Calibration;
 
   /**
-   * Setting up the ExperimentConfig, creating the simulation
-   * @param config : MetaExperimentConfig :
+   * Setting up the CalibrationConfig, creating the simulation
+   * @param config : MetaCalibrationConfig :
    */
-  setConfig(config: MetaExperimentConfig): void {
+  setConfig(config: MetaCalibrationConfig): void {
     this.name = config.name;
     this.description = config.description;
     this.simulation = new Simulation();
@@ -92,13 +92,13 @@ export default abstract class Calibration
    * Load the configuration, delivered by the TowerHandler
    */
   loadConfig(): void {
-    const config: MetaExperimentConfig =
+    const config: MetaCalibrationConfig =
       FASOW.TowerHandler.getExperimentConfig();
     this.setConfig(config);
   }
 
   /**
-   * The Strategy allow to the user to setting up configuration of Experiment doing calls to the TowerHandler
+   * The Strategy allow to the user to setting up configuration of Calibration doing calls to the TowerHandler
    * @constructor
    */
   abstract Strategy(): void;
@@ -115,15 +115,15 @@ export default abstract class Calibration
   repetition: number;
 
   /**
-   * Allows to set the repetition of the Experiment, this will hardly be called
-   * @param repetition : number : The number that indicate the actual repetition of the experiment
+   * Allows to set the repetition of the Calibration, this will hardly be called
+   * @param repetition : number : The number that indicate the actual repetition of the calibration
    */
   setRepetition(repetition: number) {
     this.repetition = repetition;
   }
 
   /**
-   * Return the Repetition of the Experiment
+   * Return the Repetition of the Calibration
    */
   getRepetition(): number {
     return this.repetition;
@@ -146,14 +146,14 @@ export default abstract class Calibration
 
   /**
    * Allows to set the max repetitions
-   * @param maxRepetition : number : The quantity of repetitions to execute the Experiment
+   * @param maxRepetition : number : The quantity of repetitions to execute the Calibration
    */
   setMaxRepetition(maxRepetition: number): void {
     this.maxRepetition = maxRepetition;
   }
 
   /**
-   * Return the max Repetitions to do the Experiment
+   * Return the max Repetitions to do the Calibration
    */
   getMaxRepetition(): number {
     return this.maxRepetition;
